171e347bdc9a9b201827bbde26611a8d87dbaf57
Pratikshya <pratikshyagochhayat@gmail.com>
2025-01-12 20:35:20 +0530

    added a text and a button in home page

--- src/Routing/Home.jsx
+++ src/Routing/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-
+import "../home.css";
 
 const Home = () => {
+  function handleButtonClick() {
+    const para = document.getElementById("paragraph");
+    para.style.color = "green";
+  }
   return (
     <>
-    <h1>Home page</h1>
-   
+      <h1>Home Page</h1>
+      <p className="para" id="paragraph">
+        This is the default page
+      </p>
+      <button onClick={handleButtonClick}>change color</button>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
